39e3f12e911125bed25d7079c13ebc82bd83c1c4
hypermetropia <sanzidul.islam155@gmail.com>
2026-01-31 11:22:56 +0600

    main menu hover sound

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,7 +6,6 @@ extends Control
 
 
 
-
 func _on_start_game_pressed() -> void:
 	audio_click.play();
 	await audio_click.finished;
@@ -21,3 +20,13 @@ func _on_exit_pressed() -> void:
 	audio_click.play();
 	await audio_click.finished;
 	get_tree().quit()
+
+
+func _on_start_game_mouse_entered() -> void:
+	audio_hover.play()
+	
+func _on_controls_mouse_entered() -> void:
+		audio_hover.play()
+		
+func _on_exit_mouse_entered() -> void:
+		audio_hover.play()
