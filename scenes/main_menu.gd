bea07a68b1b61437cd8b733a1adb2a089ed27c67
hypermetropia <sanzidul.islam155@gmail.com>
2026-01-31 11:16:36 +0600

    click sound to mainmenu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,10 +1,23 @@
 extends Control
+@onready var audio_hover: AudioStreamPlayer2D = $Audio_hover
+@onready var audio_click: AudioStreamPlayer2D = $Audio_click
+
+
+
+
+
 
 func _on_start_game_pressed() -> void:
+	audio_click.play();
+	await audio_click.finished;
 	get_tree().change_scene_to_file("res://scenes/Stroy.tscn")
 
 func _on_controls_pressed() -> void:
+	audio_click.play();
+	await audio_click.finished;
 	get_tree().change_scene_to_file("res://scenes/Controls.tscn")
 
 func _on_exit_pressed() -> void:
+	audio_click.play();
+	await audio_click.finished;
 	get_tree().quit()
